cf52e958840c2003d00776e2584adc476a9895ff
Senna de Vries <senna.de.vries@hva.nl>
2024-09-24 18:04:56 +0200

    Made shadowattacks where a platform spawns above the boss. Also made shadowplatforms that spawn underneath the player

--- Assets/Scripts/CollisionDetection.cs
+++ Assets/Scripts/CollisionDetection.cs
@@ -8,19 +8,25 @@ public class CollisionDetection : MonoBehaviour
 {
    public GameObject player;
    public GameObject gameOverText;
-   
-void OnCollisionEnter(Collision collision) 
+   public MeshRenderer meshRenderer;
+   public PlayerInput playerInput;
+
+
+   void OnCollisionEnter(Collision collision)
    {
-     if (collision.gameObject.CompareTag("LightSpot")) 
-       {
-        player.gameObject.SetActive(false);
-        gameOverText.gameObject.SetActive(true);
-       }
+      if (collision.gameObject.CompareTag("LightSpot"))
+      {
+         //player.gameObject.SetActive(false);
+         playerInput.enabled = false;
+         meshRenderer.enabled = false;
+         gameOverText.gameObject.SetActive(true);
+         
+      }
    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   // Update is called once per frame
+   void Update()
+   {
+
+   }
 }
--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
@@ -11,8 +12,12 @@ public class PlayerInput : MonoBehaviour
     private float movementX;
     private float movementY; 
     public float speed = 0;   
-    public GameObject shadow;
+    public GameObject shadowGround;
+    public GameObject shadowAttack;
     public GameObject player;
+    private Vector3 shadowPositionAdjuster = new Vector3(-1f, -0.15f, -1f);
+    private Vector3 shadowAttackPositionAdjuster = new Vector3(-1f, 2f, 0.85f);
+   
 
     // Start is called before the first frame update
     void Start()
@@ -28,10 +33,22 @@ public class PlayerInput : MonoBehaviour
 
         movementY = movementVector.y;
 
-        if(Input.GetKey(KeyCode.W))
-        {
-            
-        }
+        // if(Input.GetKey(KeyCode.W))
+        // {
+        //     shadowPositionAdjuster = new Vector3(0f, -0.15f, -1f);
+        // }
+        // if(Input.GetKey(KeyCode.A))
+        // {
+        //     shadowPositionAdjuster = new Vector3(-1f, -0.15f, -2f);
+        // }
+        // if(Input.GetKey(KeyCode.S))
+        // {
+        //     shadowPositionAdjuster = new Vector3(-2f, -0.15f, -1f);
+        // }
+        // if(Input.GetKey(KeyCode.D))
+        // {
+        //     shadowPositionAdjuster = new Vector3(-1f, -0.15f, 0f);
+        // }
     }
 
 
@@ -41,9 +58,16 @@ public class PlayerInput : MonoBehaviour
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
         playerBody.AddForce(movement * speed);
-        if(Input.GetKey(KeyCode.Mouse0))
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse1))
         {
-            Instantiate(shadow, player.transform.position + player.transform.localScale, player.transform.rotation);
+            Instantiate(shadowGround, player.transform.position + player.transform.localScale + shadowPositionAdjuster, player.transform.rotation);
+        }
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Instantiate(shadowAttack, player.transform.position + player.transform.localScale + shadowAttackPositionAdjuster, shadowAttack.transform.rotation);
         }
     }
 }
