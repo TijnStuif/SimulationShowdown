2db179d446def93a42809dc668bd8140f8c8ee81
Leonard Teekens <leonard.teekens@hva.nl>
2024-09-29 14:25:53 +0200

    (misc)[fix + refactor]: unregistered/unsubscribed from events on disable
    + reordered code alphabetically
    + created root visual element as UI parent which menus is now a child of

--- Assets/Scripts/UI/UI.cs
+++ Assets/Scripts/UI/UI.cs
@@ -1,3 +1,4 @@
+using System;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -11,41 +12,51 @@ namespace UI
         private Button m_continueGame;
         private Button m_inventory;
         private VisualElement m_menus;
+        private VisualElement m_root;
         private Button m_settings;
 
         // Start is called before the first frame update
         void Awake()
         {
-            PrototypeCat.PauseGame.GamePaused += ToggleMenus;
-            m_menus = uiDocument.rootVisualElement;
+            m_root = uiDocument.rootVisualElement;
+            m_menus = m_root.Q<VisualElement>("Menus");
             m_menus.style.display = DisplayStyle.None;
             m_continueGame = m_menus.Q<Button>("ContinueGame");
             m_inventory = m_menus.Q<Button>("Inventory");
             m_settings = m_menus.Q<Button>("Settings");
+        }
+        
+        private void OnDisable()
+        {
+            PrototypeCat.PauseGame.GamePaused -= ToggleMenus;
+            m_continueGame.UnregisterCallback<ClickEvent>(OnContinueButtonClicked);
+        }
+
+        private void OnEnable()
+        {
+            PrototypeCat.PauseGame.GamePaused += ToggleMenus;
             m_continueGame.RegisterCallback<ClickEvent>(OnContinueButtonClicked);
         }
 
-        private void ToggleMenus()
+        private void CloseMenus()
         {
-            StateManager.TogglePauseState();
-            m_menus.style.display = m_menus.style.display == DisplayStyle.None ? DisplayStyle.Flex : DisplayStyle.None;
+            StateManager.ChangePauseState(false);
+            m_menus.style.display = DisplayStyle.None;
         }
-
+        
+        private void OnContinueButtonClicked(ClickEvent e) => CloseMenus(); 
+        
         private void OpenMenus()
         { 
             StateManager.ChangePauseState(true);
             m_menus.style.display = DisplayStyle.Flex;
         }
         
-        private void CloseMenus()
+        private void ToggleMenus()
         {
-            StateManager.ChangePauseState(false);
-            m_menus.style.display = DisplayStyle.None;
+            StateManager.TogglePauseState();
+            m_menus.style.display = m_menus.style.display == DisplayStyle.None ? DisplayStyle.Flex : DisplayStyle.None;
         }
 
-        private void OnContinueButtonClicked(ClickEvent e)
-        { 
-            CloseMenus(); 
-        }
     }
 }
