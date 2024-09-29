be5a2cc8897d0114dc4d58b1404e6a18c3d8ee74
Leonard Teekens <leonard.teekens@hva.nl>
2024-09-29 17:59:59 +0200

    (#5)[feat]: Added full screen toggle to settings menu
    I tried to have the Toggle object be togglable by non mouse inputs, but I couldn't figure it out

--- Assets/Scripts/UI/UI.cs
+++ Assets/Scripts/UI/UI.cs
@@ -1,8 +1,5 @@
 // bad code starts here (this shouldn't be in one file)
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -12,7 +9,8 @@ namespace UI
     {
         [SerializeField] private UIDocument uiDocument;
 
-        private DropdownField m_fullscreen;
+        private bool m_menuActive;
+        private Toggle m_fullscreen;
         private VisualElement m_menus;
         private VisualElement m_root;
         private VisualElement m_settings;
@@ -23,8 +21,6 @@ namespace UI
         // Start is called before the first frame update
         void Awake()
         {
-            Debug.Log(UnityEngine.Device.Screen.fullScreenMode);
-            Debug.Log(Screen.fullScreenMode);
             m_root = uiDocument.rootVisualElement;
             
             m_menus = m_root.Q<VisualElement>("Menus");
@@ -33,86 +29,73 @@ namespace UI
             m_toSettings = m_menus.Q<Button>("ToSettings");
 
             m_settings = m_menus.Q<VisualElement>("Settings");
-            m_fullscreen = m_settings.Q<DropdownField>("Fullscreen");
+            m_fullscreen = m_settings.Q<Toggle>("Fullscreen");
+            m_fullscreen.value = Screen.fullScreen;
 
             m_settings.style.display = DisplayStyle.None;
             m_menus.style.display = DisplayStyle.None;
-            
-            InitFullscreenDropdown();
         }
         
         private void OnDisable()
         {
-            PrototypeCat.PauseGame.GamePaused -= ToggleMenus;
+            PrototypeCat.PauseGame.GamePaused -= MenusToggle;
             m_toContinueGame.UnregisterCallback<ClickEvent>(OnContinueButtonClicked);
             m_toSettings.UnregisterCallback<ClickEvent>(OnSettingsButtonClicked);
             m_toInventory.UnregisterCallback<ClickEvent>(OnInventoryButtonClicked);
-            // m_fullscreen.UnregisterCallback<ClickEvent>(OnFullscreenToggled);
-            m_fullscreen.UnregisterValueChangedCallback(OnFullscreenChanged);
+            m_fullscreen.UnregisterCallback<ClickEvent>(OnFullscreenToggled);
         }
 
         private void OnEnable()
         {
-            PrototypeCat.PauseGame.GamePaused += ToggleMenus;
+            PrototypeCat.PauseGame.GamePaused += MenusToggle;
             m_toContinueGame.RegisterCallback<ClickEvent>(OnContinueButtonClicked);
             m_toSettings.RegisterCallback<ClickEvent>(OnSettingsButtonClicked);
             m_toInventory.RegisterCallback<ClickEvent>(OnInventoryButtonClicked);
-            m_fullscreen.RegisterValueChangedCallback(OnFullscreenChanged);
+            m_fullscreen.RegisterCallback<ClickEvent>(OnFullscreenToggled);
         }
 
-        private void OnFullscreenChanged(ChangeEvent<string> change)
-        {
-            Debug.Log($"old: {Screen.fullScreenMode}");
-            // parse string to the FullScreenMode enum, if successful, change fullscreen mode
-            // if(Enum.TryParse(change.newValue, out FullScreenMode newEnum))
-            //     Screen.fullScreenMode = newEnum;
-            // else
-            // {
-            //     throw new Exception("parse failed ):");
-            // }
-            Screen.fullScreenMode = (FullScreenMode)m_fullscreen.index;
-            // Debug.Log($"new value: {change.newValue}");
-            // Debug.Log($"new enum: {newEnum}");
-            Debug.Log($"index: {m_fullscreen.index}");
-            Debug.Log($"new enum: {(FullScreenMode)m_fullscreen.index}");
-            Debug.Log($"new: {Screen.fullScreenMode}");
-        }
-
-        private void CloseMenus()
+        private void MenusClose()
         {
             StateManager.ChangePauseState(false);
             m_menus.style.display = DisplayStyle.None;
+            m_menuActive = false;
         }
-
-        private void InitFullscreenDropdown()
-        {
-            Func<Type, List<string>> enumToStringList = type => Enum.GetNames(type).ToList();
-            m_fullscreen.choices = enumToStringList(typeof(FullScreenMode));
-            m_fullscreen.index = Convert.ToInt32(Screen.fullScreenMode);
-        }
-        
-        private void OnContinueButtonClicked(ClickEvent e) => CloseMenus();
-        
-        private void OnInventoryButtonClicked(ClickEvent e) => Debug.Log("inventory button clicked");
-
-        private void OnSettingsButtonClicked(ClickEvent e) => ToggleSettings();
-        
-        private void OpenMenus()
+        private void MenusOpen()
         { 
             StateManager.ChangePauseState(true);
             m_menus.style.display = DisplayStyle.Flex;
+            m_menuActive = true;
         }
+        private void MenusToggle()
+        {
+            StateManager.TogglePauseState();
+            m_menus.style.display = m_menus.style.display == DisplayStyle.None ? DisplayStyle.Flex : DisplayStyle.None;
+            m_menuActive = !m_menuActive;
+        }
+        
+        private void OnContinueButtonClicked(ClickEvent e) => MenusClose();
+        
+        private void OnFullscreenToggled(ClickEvent e)
+        {
+            Debug.Log(Screen.fullScreen);
+            Screen.fullScreen = !Screen.fullScreen;
+            Debug.Log(Screen.fullScreen);
+        }
+
+        private void OnInventoryButtonClicked(ClickEvent e) => Debug.Log("inventory button clicked");
+
+        private void OnSettingsButtonClicked(ClickEvent e) => ToggleSettings();
         
         private void ToggleSettings()
         { 
             m_settings.style.display = m_settings.style.display == DisplayStyle.None ? DisplayStyle.Flex : DisplayStyle.None;
         }
-        
-        private void ToggleMenus()
-        {
-            StateManager.TogglePauseState();
-            m_menus.style.display = m_menus.style.display == DisplayStyle.None ? DisplayStyle.Flex : DisplayStyle.None;
-        }
 
+        private void Update()
+        {
+            // scuffed code to keep the checkmark/toggle
+            if (m_menuActive)
+                m_fullscreen.value = Screen.fullScreen;
+        }
     }
 }
