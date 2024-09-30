47d2af0bc5da38450142f4cedfc5659abf2e1f88
Leonard Teekens <leonard.teekens@hva.nl>
2024-09-30 07:53:10 +0200

    (#4)[broken]: attempt at programming bad guy cat ui
    used this vid as reference/guide: https://www.youtube.com/watch?v=UjkSFoLxesw
    
    I can't concentrate anymore so I'm gonna rest

---
+++ Assets/Scripts/BadguyCat/Controller.cs
@@ -0,0 +1,137 @@
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
+
+namespace BadguyCat
+{
+    public class Controller : MonoBehaviour
+    {
+        private NavMeshAgent m_agent;
+        private Transform m_player;
+
+        [SerializeField] private LayerMask whatIsGround;
+        [SerializeField] private LayerMask whatIsPlayer;
+
+        private Vector3 m_walkPoint;
+        [SerializeField] private float walkPointRange;
+        [SerializeField] private bool walkPointBeingSet;
+
+        [SerializeField] private float attackInterval;
+        private bool m_attacked;
+
+        [SerializeField] private float sightRadius;
+        [SerializeField] private float attackRadius;
+        private bool m_inSightRange;
+        private bool m_inAttackRange;
+
+        private int PlayerLayer => whatIsPlayer;
+        private int GroundLayer => whatIsGround;
+
+        private void Awake()
+        {
+            m_agent = GetComponent<NavMeshAgent>();
+            m_player = GameObject.Find("PrototypeCat").transform;
+            m_walkPoint = transform.position;
+        }
+
+        private void FixedUpdate()
+        {
+            // circle collision between our given range, and the player
+            m_inSightRange = Physics.CheckSphere(transform.position, sightRadius, PlayerLayer);
+            m_inAttackRange = Physics.CheckSphere(transform.position, attackRadius, PlayerLayer);
+
+            if (!m_inSightRange)
+            {
+                PlayerUndetected();
+                return;
+            }
+
+            if (!m_inAttackRange)
+            {
+                PlayerMoveTowards();
+                return;
+            }
+
+            PlayerAttack();
+        }
+
+        private Task SetWalkPoint()
+        {
+            // keep trying to set a walk point until it is on the ground
+            do
+            {
+                // get two random offsets 
+                float randomX = Random.Range(-walkPointRange, walkPointRange);
+                float randomZ = Random.Range(-walkPointRange, walkPointRange);
+                // use for new walk point
+                m_walkPoint = new Vector3(transform.position.x + randomX, transform.position.y,
+                    transform.position.z + randomZ);
+            } while (Physics.Raycast(m_walkPoint, -transform.up, 2f, GroundLayer));
+
+            // if walk point invalid, return false, this would repeat the function due to the while loop
+            // I think it would be cool if it could map out what is not the ground if it's called too many times, but
+            // no time for that
+            return Task.CompletedTask;
+        }
+
+        private void ResetWalkPoint()
+        {
+            m_walkPoint = transform.position;
+        }
+
+        private async void PlayerUndetected()
+        {
+            if (walkPointBeingSet) return;
+            if (m_walkPoint == transform.position)
+            {
+                walkPointBeingSet = true;
+                // multi threading the WalkPoint setting so the game won't freeze in case of too many attempts
+                await SetWalkPoint();
+                m_agent.SetDestination(m_walkPoint);
+                walkPointBeingSet = false;
+                // returning so that two methods won't be able to try the same thing at the same time
+                // the async functionality did its job, let's kill the async thread and let the regular synchronous calls
+                // do their simple synchronous things
+                return;
+            }
+
+            var distance = transform.position - m_walkPoint;
+            if (distance.magnitude < 1f)
+            {
+                // stand still and find another walk point
+                ResetWalkPoint();
+                m_agent.SetDestination(m_walkPoint);
+            }
+        }
+
+        private void PlayerMoveTowards() => m_agent.SetDestination(m_player.position);
+
+        private void PlayerAttack()
+        {
+            if (m_attacked) return;
+            
+            m_agent.SetDestination(transform.position);
+            
+            // attack code here
+            Debug.Log("attacking!!!111");
+            m_attacked = true;
+            //
+            
+            // I thought maybe I could rewrite this for fun, but it calls an extern, so it's probably done in C++
+            transform.LookAt(m_player);
+            
+            // this looks like literally the coolest thing ever,
+            // but it uses reflection so it's very expensive
+            Invoke(nameof(ResetAttack), attackInterval);
+        }
+
+        private void ResetAttack()
+        {
+            Debug.Log("stopping attack");
+            m_attacked = false;
+            // reset attack code here
+
+        }
+    }
+}
