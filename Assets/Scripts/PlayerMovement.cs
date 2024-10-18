caff0452f97d68e4f86105770f04708d492f36a3
Tijn <tijnstuif@gmail.com>
2024-10-18 12:29:32 +0200

    added jump and teleport functionality to the player, which completes their moveset

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,25 +1,23 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Vector2 movementInput = Vector2.zero;
+    public Vector2 movementInput = Vector2.zero;
+    public bool isGrounded;
     private Vector3 movement;
+    private float groundDrag = 0.2f;
     [SerializeField] private Rigidbody player;
     [SerializeField] private float speed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private LayerMask groundMask;
+    private float speedControlMultiplier = 0.4f;
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         movement = new Vector3(movementInput.x, 0, movementInput.y);
         player.AddForce(speed * Time.fixedDeltaTime * movement.normalized, ForceMode.Impulse); 
         SpeedControl();
+        CheckForDrag();
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -38,10 +36,26 @@ public class PlayerMovement : MonoBehaviour
     private void SpeedControl()
     {
         Vector3 horizontalVelocity = new(player.velocity.x, 0, player.velocity.z);
-        if (horizontalVelocity.magnitude > (speed / 4))
+
+        if (horizontalVelocity.magnitude > (speed * speedControlMultiplier))
         {
-            Vector3 cappedVelocity = horizontalVelocity.normalized * (speed / 4);
+            Vector3 cappedVelocity = horizontalVelocity.normalized * (speed * speedControlMultiplier);
             player.velocity = new Vector3(cappedVelocity.x, player.velocity.y, cappedVelocity.z);
+        }
+    }
+
+    //this function checks for ground distance for the player and then applies drag if it does touch the ground
+    private void CheckForDrag()
+    {
+        isGrounded = Physics.Raycast(player.transform.position, Vector3.down, 1.1f, groundMask);
+        
+        if (isGrounded)
+        {
+            player.drag = groundDrag;
+        }
+        else
+        {
+            player.drag = 0;
         }
     }
 }
