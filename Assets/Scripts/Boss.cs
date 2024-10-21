c8ce1d20742424ac8f4cf6363d3d31c64898aeff
Senna de Vries <senna.de.vries@hva.nl>
2024-10-21 16:05:58 +0200

    Tried to make a HUD with UI Toolkit and made a variable for the DashCooldown...

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public HealthBar healthBar;
+    private int currentHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(10);
+        }
+    }
+
+    // Update is called once per frame
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+}
