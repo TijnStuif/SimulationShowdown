4ed0684cc7ac165f3705ff9b7a14b31acd02d249
Leonard Teekens <leonard.teekens@hva.nl>
2024-10-21 18:06:54 +0200

    (#15) [feat]: pause menu can pop-up

--- Assets/Scripts/GameControllerScript.cs
+++ Assets/Scripts/GameControllerScript.cs
@@ -1,7 +1,7 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class GameControllerScript : MonoBehaviour
 {
@@ -9,11 +9,16 @@ public class GameControllerScript : MonoBehaviour
     private List<IAttack> environmentAttacks;
     private List<IAttack> directAttacks;
     private float timer;
+    private bool gamePaused;
+    private GameObject pauseMenu;
     public float minAttackInterval = 5f; 
-    public float maxAttackInterval = 15f; 
+    public float maxAttackInterval = 15f;
+    [SerializeField] private GameObject pauseMenuFab;
 
     void Start()
     {
+        pauseMenu = Instantiate(pauseMenuFab);
+        pauseMenu.SetActive(false);
         // Gets all the objects in the scene that have the IAttack interface
         attacks = FindObjectsOfType<MonoBehaviour>().OfType<IAttack>().ToList();
         
@@ -26,7 +31,6 @@ public class GameControllerScript : MonoBehaviour
         // list of attacks per type
         environmentAttacks = attacks.Where(attack => attack.Type == AttackType.Environment).ToList();
         directAttacks = attacks.Where(attack => attack.Type == AttackType.Direct).ToList();
-
 
         SetRandomInterval(); 
     }
@@ -54,6 +58,20 @@ public class GameControllerScript : MonoBehaviour
         attacks[randomIndex].Execute();
         Debug.Log($"Executed attack: {attacks[randomIndex].GetType().Name}");
     }
+
+    private void OnPause()
+    {
+        if (gamePaused)
+        {
+            pauseMenu.SetActive(false);
+            gamePaused = false;
+            return;
+        }
+
+        pauseMenu.SetActive(true);
+        gamePaused = true;
+    }
+    
 
     private void SetRandomInterval()
     {
