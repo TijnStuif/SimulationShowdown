c8ad47d119a0aa1784a69c427720eb15574617ef
Leonard Teekens <leonard.teekens@hva.nl>
2024-10-22 03:37:32 +0200

    (#12) [feature]: added game over functionality added the code for it, uxml, uss, and a prefab succesfully avoided modifying the scene doing so

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,13 +2,27 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using Cursor = UnityEngine.Cursor;
 
 public class Player : MonoBehaviour
 {
     public int maxHealth = 100;
     public HealthBar healthBar;
     private int currentHealth;
-   
+    private GameObject gameOverFab;
+    private UIDocument gameOverDocument;
+    private bool lost = false;
+
+    private void Awake()
+    {
+        // loading resource like this so I don't need to modify the scene
+        // resources can be loaded like this when your resources is in the Assets/Resources folder
+        gameOverFab = Resources.Load<GameObject>("Game Over Screen");
+        gameOverDocument = Instantiate(gameOverFab).GetComponent<UIDocument>();
+        gameOverDocument.rootVisualElement.AddToClassList("hidden");
+    }
+
     void Start()
     {
         currentHealth = maxHealth;
@@ -24,5 +38,17 @@ public class Player : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         TakeDamage(10);
+    }
+
+    private void Update()
+    {
+        if (currentHealth <= 0 && lost == false)
+        {
+            lost = true;
+            Time.timeScale = 0;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            gameOverDocument.rootVisualElement.RemoveFromClassList("hidden");
+        }
     }
 }
