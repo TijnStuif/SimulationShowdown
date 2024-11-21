737a902186cd221f8d91ece9dff712bb55a7920d
Leonard Teekens <leonard.teekens@hva.nl>
2024-11-21 16:53:38 +0100

    (#32) refactor[Teleport.cs]: split methods, added booleans + enum + event
    Easier to work with especially with the boolean property which invokes
    an event when set though this needs to unfortunately be done in Update
    since the visual indicator needs to be updated before teleporting

--- Assets/Scripts/Player/Teleport.cs
+++ Assets/Scripts/Player/Teleport.cs
@@ -1,5 +1,4 @@
-using System.Linq;
-using Cinemachine;
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,6 +6,14 @@ namespace Player
 {
     public class Teleport : MonoBehaviour
     {
+        public enum BossRange
+        {
+           Inside,
+           Outside
+        }
+        
+        public static event Action<BossRange> RangeChange;
+        
         [SerializeField] private Rigidbody player;
         [SerializeField] private Movement playerMovement;
         
@@ -17,6 +24,41 @@ namespace Player
         private float timeSinceLastTeleport = 0;
         private readonly float aimThreshold = 30f; 
         private readonly float maxTeleportDistance = 10f;
+        
+        private Vector3 InputMovement3D { get; set; }
+
+
+        private bool m_inAttackRange;
+        
+        private Vector3 DirectionToBoss => (boss.position - player.position).normalized;
+
+        private bool InAttackRange
+        {
+            get => m_inAttackRange;
+            set
+            {
+                if (InAttackRange == value)
+                    throw new InvalidOperationException("ERROR: Please do not adjust this value carelessly, " +
+                                                        "it invokes an event\n"+
+                                                        "please only call this setter if the new value is different");
+                if (Debug.isDebugBuild)
+                    Debug.Log($"Attack range: {value}");
+                
+                switch (value)
+                {
+                    case true:
+                        RangeChange?.Invoke(BossRange.Inside);
+                        break;
+                    case false:
+                        RangeChange?.Invoke(BossRange.Outside);
+                        break;
+                }
+
+                m_inAttackRange = value;
+            }
+        }
+
+        private bool CanTeleport => Time.time - timeSinceLastTeleport >= teleportCooldown;
 
         private void Awake()
         {
@@ -27,40 +69,55 @@ namespace Player
             playerCamera = GameObject.Find("PlayerCamera").GetComponent<Camera>();
         }
 
+        private void UpdateAttackRange()
+        {
+            float angleToBoss = Vector3.Angle(playerCamera.transform.forward, DirectionToBoss);
+
+            var newRange = (angleToBoss <= aimThreshold);
+            if (InAttackRange == newRange)
+                return;
+            InAttackRange = newRange;
+        }
+
+        private void StandardTeleport()
+        {
+                if (Physics.Raycast(origin: player.position, direction: InputMovement3D, hitInfo: out RaycastHit hit, 
+                        maxDistance: maxTeleportDistance))
+                { 
+                    player.position = hit.point - InputMovement3D * 0.5f; 
+                }
+                else
+                { 
+                    player.position += InputMovement3D * maxTeleportDistance;
+                }
+            
+        }
+
+        private bool AttackingBoss()
+        {
+            if (!(InputMovement3D.z > 0)) return false;
+            UpdateAttackRange();
+            if (!InAttackRange) return false;
+            float distanceToBoss = Vector3.Distance(player.position, boss.position);
+
+            player.position += DirectionToBoss * distanceToBoss;
+            return true;
+        }
+
         public void OnTeleport(InputAction.CallbackContext context)
         {
             if (context.performed)
             {
-                if (Time.time - timeSinceLastTeleport < teleportCooldown) return;
+                if (!CanTeleport) return;
+                
                 timeSinceLastTeleport = Time.time;
                 player.velocity = Vector3.zero;
 
-                Vector3 movementInput = new Vector3(playerMovement.movementInput.x, 0, playerMovement.movementInput.y).normalized;
+                InputMovement3D = new Vector3(playerMovement.movementInput.x, 0, playerMovement.movementInput.y).normalized;
 
-                if (movementInput.z > 0)
-                {
-                    Vector3 directionToBoss = (boss.position - player.position).normalized;
+                if (AttackingBoss()) return;
 
-                    float angleToBoss = Vector3.Angle(playerCamera.transform.forward, directionToBoss);
-
-                    if (angleToBoss <= aimThreshold)
-                    {
-                        float distanceToBoss = Vector3.Distance(player.position, boss.position);
-
-                        player.position += directionToBoss * distanceToBoss;
-                        return;
-                    }
-                }
-
-                RaycastHit hit;
-                if (Physics.Raycast(player.position, movementInput, out hit, maxTeleportDistance))
-                {
-                    player.position = hit.point - movementInput * 0.5f; 
-                }
-                else
-                {
-                    player.position += movementInput * maxTeleportDistance;
-                }
+                StandardTeleport();
             }
         }
     }
