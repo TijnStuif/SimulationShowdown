8eef08fef23ea8bb887c943f2807b2675c383f33
Tijn <tijnstuif@gmail.com>
2024-11-24 20:49:29 +0100

    made an indicator for the input reverser attack, and also changed input visualizer colour to be purple when reversed

--- Assets/Scripts/Boss/Attack/InvertedControls.cs
+++ Assets/Scripts/Boss/Attack/InvertedControls.cs
@@ -8,22 +8,36 @@ namespace Boss.Attack
         public Type Type => Type.Environment;
 
         private Movement playerMovement;
+        private GameObject player;
+        [SerializeField] private GameObject inputReverserAttackIndicatorPrefab;
+        private GameObject inputReverserAttackIndicator;
+        private Vector3 startPosition = new Vector3(200, 0, -20);
 
         void Start()
         {
+            inputReverserAttackIndicator = Instantiate(inputReverserAttackIndicatorPrefab);
+            player = FindObjectOfType<Player.Controller>().gameObject;
             playerMovement = FindObjectOfType<Movement>();
+            inputReverserAttackIndicator.transform.position = startPosition;
         }
 
         public void Execute()
         {
-            if (playerMovement != null)
+            if (playerMovement != null || player != null)
             {
+                inputReverserAttackIndicator.transform.position = player.transform.position;
+                Invoke(nameof(RemoveIndicator), 0.2f);
                 playerMovement.areControlsInverted = !playerMovement.areControlsInverted;
             }
             else
             {
                 Debug.LogWarning("PlayerMovement component not found.");
             }
+        }
+
+        private void RemoveIndicator()
+        {
+            inputReverserAttackIndicator.transform.position = startPosition;
         }
     }
 }
