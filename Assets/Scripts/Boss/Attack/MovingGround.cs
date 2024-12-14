56c610dc495207050bd9596c1bea7a9777eaaabc
Senna de Vries <senna.de.vries@hva.nl>
2024-12-14 17:57:17 +0100

    Tiles move up and use of enums succeeded but code isnt working yet

--- Assets/Scripts/Boss/Attack/MovingGround.cs
+++ Assets/Scripts/Boss/Attack/MovingGround.cs
@@ -4,39 +4,52 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 using UnityEngine.UIElements;
 using Random = UnityEngine.Random;
 
 public class MovingGround : MonoBehaviour
 {
 
-    private HashSet<GameObject> movingFloorTiles = new HashSet<GameObject>();
-    private GameObject movingFloor;
-    private List<GameObject> floorTiles = new List<GameObject>();
+    private List<(Transform, State)> movingFloorTiles = new List<(Transform, State)>();
+    private (Transform, State) movingFloor;
+    private List<(Transform, State)> floorTiles = new List<(Transform, State)>();
     private List<GameObject> UpTiles = new List<GameObject>();
     private List<GameObject> DownTiles = new List<GameObject>();
+    private List<(Transform, State)> floorTilesToRemove = new List<(Transform, State)>(); 
     [SerializeField] private Material indicatorMaterial;
-    private int movementSwap = 0;
    
+   private enum State
+   {
+    Down,
+    Middle,
+    Up
+   }
 
 
     void Start()
     {
-        floorTiles = new List<GameObject>(GameObject.FindGameObjectsWithTag("SimulationBorder"));
+        floorTiles = new List<(Transform, State)>();
+
+        var tiles = new List<GameObject>(GameObject.FindGameObjectsWithTag("SimulationBorder"));
+
+        foreach(var tile in tiles)
+        {
+            var tuple = (tile.transform, State.Down);
+            floorTiles.Add(tuple);
+        }
 
         StartCoroutine(SelectingFloorTile());
 
-        movementSwap = 1;
     }
 
     private IEnumerator SelectingFloorTile()
     {
         Debug.Log("Coroutine starts");
         movingFloor = floorTiles[Random.Range(0, floorTiles.Count)];
-        DownTiles.Add(movingFloor);
-        floorTiles.Remove(movingFloor);
+        movingFloorTiles.Add(movingFloor);
 
-        
+        movingFloorTiles[i].Item1.gameObject.GetComponent<MeshRenderer>().material = indicatorMaterial;
 
         yield return new WaitForSeconds(6f);
 
@@ -45,49 +58,38 @@ public class MovingGround : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if(movementSwap == 1)
+        for (int i = 0; i < movingFloorTiles.Count; i++)
         {
-            StartCoroutine(MoveTileUp());
+            floorTilesToRemove.Add(movingFloorTiles[i]);
+
+            if(movingFloorTiles[i].Item2 == State.Down)
+            {
+               movingFloorTiles[i].Item1.transform.position = Vector3.MoveTowards(movingFloorTiles[i].Item1.position, new Vector3(movingFloorTiles[i].Item1.position.x, 2.45f, movingFloorTiles[i].Item1.position.z), 1f * Time.deltaTime); 
+
+               if(movingFloorTiles[i].Item1.position.y == 2.45f)
+               {
+                    
+                    movingFloorTiles[i].Item2 = State.Middle;
+               }
+            } 
+
+            if(movingFloorTiles[i].Item2 == State.Middle)
+            {
+               movingFloorTiles[i].Item1.transform.position = Vector3.MoveTowards(movingFloorTiles[i].Item1.position, new Vector3(movingFloorTiles[i].Item1.position.x, 2.45f, movingFloorTiles[i].Item1.position.z), 1f * Time.deltaTime); 
+            } 
+
+            if(movingFloorTiles[i].Item2 == State.Up)
+            {
+               movingFloorTiles[i].Item1.transform.position = Vector3.MoveTowards(movingFloorTiles[i].Item1.position, new Vector3(movingFloorTiles[i].Item1.position.x, 2.45f, movingFloorTiles[i].Item1.position.z), 1f * Time.deltaTime); 
+            } 
         }
-        if(movementSwap == 2)
-        {
-            StartCoroutine(MoveTileDown());
-        }
-    }
-
-
-    private IEnumerator MoveTileUp()
-    {
-        foreach (var movingFloor in DownTiles)
-        {
-            
-            movingFloor.GetComponent<MeshRenderer>().material = indicatorMaterial;
-
-            yield return new WaitForSeconds(1f);
-
-            movingFloor.transform.position = Vector3.MoveTowards(movingFloor.transform.position, new Vector3(movingFloor.transform.position.x, 2.45f, movingFloor.transform.position.z), 1f * Time.deltaTime);
-            
-
-            yield return new WaitForSeconds(12f);
-
-            UpTiles.Add(movingFloor);
-            DownTiles.Remove(movingFloor);
-            movementSwap = 2;
-        }
-    }
-
-    private IEnumerator MoveTileDown()
-    {
-        foreach (var movingFloor in UpTiles)
-        {
-            movingFloor.transform.position = Vector3.MoveTowards(movingFloor.transform.position, new Vector3(movingFloor.transform.position.x, 0, movingFloor.transform.position.z), 1f * Time.deltaTime);
         
-            yield return new WaitForSeconds(12f);
-
-            DownTiles.Add(movingFloor);
-            UpTiles.Remove(movingFloor);
-            movementSwap = 1;
+        foreach (var tileToRemove in floorTilesToRemove)
+        {
+            movingFloorTiles.Remove(tileToRemove);
         }
+        floorTilesToRemove.Clear();
     }
+
 
 }
