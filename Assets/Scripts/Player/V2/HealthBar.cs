e59811237f68eb833acea19e67e0483eb941a70d
Leonard Teekens <leonard.teekens@hva.nl>
2024-12-15 15:36:02 +0100

    WIP: imported old Controller and Healthbar from V1

---
+++ Assets/Scripts/Player/V2/HealthBar.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+// copy-pasted from V1
+namespace Player.V2
+{
+    public class HealthBar : MonoBehaviour
+    {
+        VisualElement m_uiDocument;
+        public Transform player;
+        VisualElement healthbar;
+        ProgressBar progressbar;
+        Label playertext;
+        private Camera cam;
+        public UIDocument healthbarDocument;
+    
+        private void Awake()
+        {
+            cam = GameObject.Find("PlayerCamera").GetComponent<Camera>();
+            
+            //acccess the UI document
+            m_uiDocument = GetComponent<UIDocument>().rootVisualElement;
+
+            //access the healthbar VisualElement and the progressBar
+            healthbar = m_uiDocument.Q<VisualElement>("healthbar");
+            progressbar = m_uiDocument.Q<ProgressBar>("healthbar");
+            playertext = m_uiDocument.Q<Label>("Playertext");
+
+            m_uiDocument.Add(healthbar);
+
+            healthbar.style.width = 500;
+            healthbar.style.left = -580;
+
+            playertext.style.width = 500;
+            playertext.style.top = 940;
+            playertext.style.left = -580;
+
+        }
+
+        private void Update()
+        {
+            //Access the health of the player from the player script
+            int health = GameObject.Find("Player").GetComponent<Controller>().currentHealth;
+
+            progressbar.value = health;
+        }
+    }
+}
