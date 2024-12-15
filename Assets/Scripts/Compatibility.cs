652b34ccbb56098a9a6ad487b61480d313e8693e
Leonard Teekens <leonard.teekens@hva.nl>
2024-12-15 18:21:32 +0100

    fix: Removed last bit of V1 compatiblity code

--- Assets/Scripts/Compatibility.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine.Rendering;
-using UnityEngine.SceneManagement;
-
-public static class Compatibility
-{
-    public static bool IsV1 => SceneManager.GetActiveScene().name == "GameSceneV1";
-}
