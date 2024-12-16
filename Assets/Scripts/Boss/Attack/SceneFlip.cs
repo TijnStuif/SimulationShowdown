58dd5155261fd37beebf61fb312e93fd4852db8b
Leonard Teekens <leonard.teekens@hva.nl>
2024-12-16 02:37:09 +0100

    feature[Boss/Attack/SceneFlip]: screen flip works!

--- Assets/Scripts/Boss/Attack/SceneFlip.cs
+++ Assets/Scripts/Boss/Attack/SceneFlip.cs
@@ -1,95 +1,78 @@
 using System;
 using UnityEngine;
 using System.Collections;
+using Cinemachine;
+using UnityEngine.Serialization;
 
 namespace Boss.Attack
 {
     public class SceneFlip : MonoBehaviour, IAttack
     {
         public Type Type => Type.Environment;
+        /// <summary>
+        /// Both the amount of time before flipping (since indicator)
+        /// </summary>
+        [SerializeField] private float m_indicatorTime = 2f;
+        /// <summary>
+        /// Amount of time the screen flip lasts
+        /// </summary>
+        [SerializeField] private float m_flippedTime = 3f;
 
-        private Transform playerCamera;
-        private ParticleSystem indicatorParticle;
-        private bool isFlipped = false;
-        AudioManager audioManager;
+        private AudioManager m_audioManager;
+        private CinemachineFreeLook m_cinemachineFreeLook;
+        private ParticleSystem m_indicatorParticle;
 
         private void Awake()
         {
-            // MonoBehaviour playerScript = null;
-            // #if DEBUG
-            // if (Compatibility.IsV1)
-            // {
-            //     playerScript = FindObjectOfType<Player.V2.Controller>();
-            // }
-            // #endif
-            // if (playerScript == null)
-            //     playerScript = FindObjectOfType<Player.V2.Movement>();
-            // if (playerScript == null)
-            //     throw new StateController.ScriptNotFoundException(nameof(playerScript));
-            // var player = playerScript.gameObject;
-            // 
-            // #if DEBUG
-            // if (Compatibility.IsV1)
-            // {
-            //     // find PlayerFollower (this object is under Player)
-            //     var rotationScript = player.GetComponentInChildren<Rotation>();
-            //     if (rotationScript == null)
-            //         throw new StateController.ScriptNotFoundException(nameof(rotationScript));
-            //     playerCamera = rotationScript.gameObject.transform;
-            // }
-            // #endif
-            // GameObject player = GameObject.Find("Player");
-            // if (player != null)
-            // {
-            //     // Find the ParticleSystem component on the player GameObject
-                    // if (indicatorParticle == null) 
-                    //     throw new NullReferenceException("ERROR: could not find Particle System component");
+            m_indicatorParticle = FindObjectOfType<Player.V2.Controller>()
+                .gameObject
+                .GetComponentInChildren<ParticleSystem>();
+            if (m_indicatorParticle == null)
+                throw new NullReferenceException("ERROR: indicator particle system not found");
+            
+            m_cinemachineFreeLook = FindObjectOfType<CinemachineFreeLook>();
+            if (m_cinemachineFreeLook == null)
+                throw new NullReferenceException("ERROR: CM FreeLook camera not found");
+            
+            m_audioManager = FindObjectOfType<AudioManager>();
+            if (m_audioManager == null)
+                throw new NullReferenceException("ERROR: AudioManager not found");
+        }
 
-            //     // Disable Play On Awake
-            //     var main = indicatorParticle.main;
-            //     main.playOnAwake = false;
-
-            //     // Stop the particle system
-            //     indicatorParticle.Stop();
-            // }
-            // audioManager = FindObjectOfType<AudioManager>();
+        private void Start()
+        {
         }
 
         public void Execute()
         {
-            // #if DEBUG
-            // if (Compatibility.IsV1)
-            //     audioManager.PlaySFX(audioManager.bossSceneFlipSFX);
-            //     StartCoroutine(ActivateSceneFlip());
-            // else
-            //     Debug.Log("Scene flip is currently not implemented for Player V2");
-            // #endif
+                m_audioManager.PlaySFX(m_audioManager.bossSceneFlipSFX);
+                StartCoroutine(ActivateSceneFlip());
         }
 
         private IEnumerator ActivateSceneFlip()
         {
-            //change the color of the particle to yellow
-            var main = indicatorParticle.main;
+            // Change the color of the particle to yellow
+            var main = m_indicatorParticle.main;
             main.startColor = new Color(1, 1, 0, 1);
             // Play the particle system
-            indicatorParticle.Play();
-
-            // Wait for 2 seconds
-            yield return new WaitForSeconds(2f);
-
-            // Change the gravity
-            if (isFlipped)
-            {
-                playerCamera.localRotation = Quaternion.Euler(0, 0, 0);
-                playerCamera.position = new Vector3(playerCamera.position.x, playerCamera.position.y - 2, playerCamera.position.z);
-            }
-            else
-            {
-                 playerCamera.localRotation =  Quaternion.Euler(0, 0, 180);
-                    playerCamera.position = new Vector3(playerCamera.position.x, playerCamera.position.y + 2, playerCamera.position.z);
-            }
-
-                isFlipped = !isFlipped;
+            m_indicatorParticle.Play();
+            // Wait
+            yield return new WaitForSeconds(m_indicatorTime);
+            
+            // ggs
+            // Flip camera 180 degrees
+            m_cinemachineFreeLook.m_Lens.Dutch = 180f;
+            // Wait
+            yield return new WaitForSeconds(m_flippedTime);
+            
+            // Play particles again
+            main.startColor = new Color(1, 1, 0, 1);
+            m_indicatorParticle.Play();
+            // Wait
+            yield return new WaitForSeconds(m_indicatorTime);
+            
+            // Return camera to regular rotation
+            m_cinemachineFreeLook.m_Lens.Dutch = 0f;
         }
     }
 }
