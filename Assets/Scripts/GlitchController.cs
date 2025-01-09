4972f938b78998d32be4da0d057f522cdc9e105a
Tijn <tijnstuif@gmail.com>
2025-01-09 14:12:35 +0100

    created a very quick animation for the boss when the fight starts. Next step is to glitch out the player's view before the boss enters so it looks like the boss glitched the view to enter the area

---
+++ Assets/Scripts/GlitchController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlitchController : MonoBehaviour
+{
+
+    [SerializeField] private Material glitchMaterial;
+    [SerializeField] private float noiseAmount;
+    [SerializeField] private float glitchStrength;
+    [SerializeField] private float scanLineStrength;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
