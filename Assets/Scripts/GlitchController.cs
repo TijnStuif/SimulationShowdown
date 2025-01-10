5b50ce5cac9cec5943a5287aaa61a9d4fe34e080
Tijn <tijnstuif@gmail.com>
2025-01-10 21:07:33 +0100

    added sound effect to the start cutscene, tried out camera shake but was not needed because of the glitchy nature of the cutscene already

--- Assets/Scripts/GlitchController.cs
+++ Assets/Scripts/GlitchController.cs
@@ -2,29 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+
 namespace Player.V2
 {
     public class GlitchController : MonoBehaviour
     {
         [SerializeField] private Material glitchMaterial;
-        private float noiseAmount = 30f;
-        private float glitchStrength = 1f;
+        private float noiseAmount = 0;
+        private float glitchStrength = 0;
         private float scanLineStrength = 0;
-        private float flickerStrength = 0.05f;
+        private float flickerStrength = 0;
         private float baseNoiseAmount = 30f;
         private float baseGlitchStrength = 1f;
         private float baseScanLineStrength = 0;
         private float baseFlickerStrength = 0.05f;
         private V2.Controller playerController;
         private PhaseController phaseController;
-        // Start is called before the first frame update
+        private AudioManager audioManager;
+
         void OnEnable()
         {
             playerController = GetComponent<V2.Controller>();
             phaseController = FindObjectOfType<PhaseController>();
+            audioManager = FindObjectOfType<AudioManager>();
             phaseController.phaseChanged.AddListener(() => GlitchOnPhase());
             UpdateAllGlitchValues();
-            Invoke(nameof(StartCutsceneValues), 1.5f);
+            Invoke(nameof(StartCutsceneValues), 3f);
         }
 
         void GlitchOnPhase()
@@ -33,6 +36,7 @@ namespace Player.V2
             glitchStrength += 1;
             flickerStrength += 0.05f;
             UpdateAllGlitchValues();
+            audioManager.PlaySFX(audioManager.bossGlitchSFX);
         }
 
         void UpdateAllGlitchValues()
@@ -49,7 +53,8 @@ namespace Player.V2
             glitchStrength = 10;
             flickerStrength = 0.5f;
             UpdateAllGlitchValues();
-            Invoke(nameof(NormalValues), 2f);
+            audioManager.PlaySFX(audioManager.bossGlitchSFX);
+            Invoke(nameof(NormalValues), 2.5f);
         }
 
         void NormalValues()
