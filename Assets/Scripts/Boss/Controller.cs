d6d276a747b84caeb56afe4e547d193f5110afc2
Senna de Vries <senna.de.vries@hva.nl>
2025-01-13 16:57:35 +0100

    Added a cheat button to damage the boss

--- Assets/Scripts/Boss/Controller.cs
+++ Assets/Scripts/Boss/Controller.cs
@@ -41,6 +41,10 @@ namespace Boss
             {
                 invincibilityFrames += 1;
             }
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                OnTeleportOnBossAttacked(20);
+            }
         }
     
         public void UnlockDamage() => damageLock = false;
